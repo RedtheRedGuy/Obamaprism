f9e22faa1797db69ee641fe802e1cc26bd3da4a6
RedtheRedGuy <186442544+RedtheRedGuy@users.noreply.github.com>
2025-03-05 09:35:27 +0700

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,24 @@
+document.addEventListener("click", function() {
+        var audio = document.getElementById("bg-music");
+        if (audio.muted) {
+            audio.muted = false;
+            audio.play();
+        }
+    }, { once: true });
+
+    const slider = document.getElementById('speedSlider');
+    const speedValue = document.getElementById('speedValue');
+    const pyramid = document.querySelector('.pyramid');
+    const changeTile1Button = document.getElementById('changeTile1');
+    const changeTile2Button = document.getElementById('changeTile2'); 
+    const changeTile3Button = document.getElementById('changeTile3');
+    
+    slider.addEventListener('input', () => {
+        const speed = slider.value;
+        speedValue.textContent = speed;
+        pyramid.style.animationDuration = `${3 / speed}s`;
+    });
+
+    changeTile1Button.addEventListener('click', () => { document.querySelector('.front').style.backgroundImage = "url('tile001.png')"; }); 
+    changeTile2Button.addEventListener('click', () => { document.querySelector('.front').style.backgroundImage = "url('tile002.png')"; }); 
+    changeTile3Button.addEventListener('click', () => { document.querySelector('.front').style.backgroundImage = "url('tile003.png')"; });
