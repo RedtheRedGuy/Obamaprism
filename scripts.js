e28d0f2b525376f9b5d02ebf36a19d863d820af4
RedtheRedGuy <186442544+RedtheRedGuy@users.noreply.github.com>
2025-03-06 09:53:04 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -34,13 +34,36 @@ const changeTile = (button, tile) => {
         });
     }
 };
-document.getElementById("fullscreenBtn").addEventListener("click", function() { 
-    if (!document.fullscreenElement) { 
-        document.documentElement.requestFullscreen();
+document.getElementById("fullscreenBtn").addEventListener("click", function() {
+    if (!document.fullscreenElement &&    // Check if fullscreen is not active
+        !document.webkitFullscreenElement && 
+        !document.mozFullScreenElement && 
+        !document.msFullscreenElement) {
+        
+        // Request fullscreen for different browsers
+        if (document.documentElement.requestFullscreen) {
+            document.documentElement.requestFullscreen();
+        } else if (document.documentElement.webkitRequestFullscreen) { // Safari
+            document.documentElement.webkitRequestFullscreen();
+        } else if (document.documentElement.mozRequestFullScreen) { // Firefox
+            document.documentElement.mozRequestFullScreen();
+        } else if (document.documentElement.msRequestFullscreen) { // IE/Edge
+            document.documentElement.msRequestFullscreen();
+        }
     } else {
-        document.exitFullscreen(); 
-    } 
+        // Exit fullscreen for different browsers
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.webkitExitFullscreen) { // Safari
+            document.webkitExitFullscreen();
+        } else if (document.mozCancelFullScreen) { // Firefox
+            document.mozCancelFullScreen();
+        } else if (document.msExitFullscreen) { // IE/Edge
+            document.msExitFullscreen();
+        }
+    }
 });
+
 changeTile(changeTile1Button, 'tile001.png');
 changeTile(changeTile2Button, 'tile002.png');
 changeTile(changeTile3Button, 'tile003.png');
